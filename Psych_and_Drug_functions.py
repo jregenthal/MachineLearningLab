cfe1bf4a424e5c514581f7468a23ac58a53da941
sonnenthaler <62459892+sonnenthaler@users.noreply.github.com>
2021-01-19 09:36:44 +0100

    File for Functions

---
+++ Psych_and_Drug_functions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Course: Machine Learning Lab given by Prof. Brefeld
+Project: Influence of psychological factors on drug consumption
+Authors: Johanna Regenthal and Sofija Engelson
+Due date: 
+'''
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, KBinsDiscretizer
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from scipy.stats import chi2_contingency
+import networkx
+from itertools import combinations
+import math, copy
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import patches as mpatches
+import Psych_and_Drug_functions as PDfunc
+
+# ---------- 1.Reading and cleaning data -------------------------------------
+
+
+
+
+# ---------- 2. Exploratory analysis -----------------------------------------
+
+
+
